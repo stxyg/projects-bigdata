62c3bd948f756e379318d97cef22b2b324da6f3e
ningning.cheng <ningning.cheng@100credit.com>
2021-05-06 00:03:08 +0800

    add

---
+++ test-spark/spark-core/src/main/java/com/cnn/bigdata/spark/core/Test.scala
@@ -0,0 +1,11 @@
+package com.cnn.bigdata.spark.core
+
+/**
+ * @author Administrator
+ * @date 2021/5/5
+ */
+class Test {
+  def main(args: Array[String]): Unit = {
+    println("hello spark")
+  }
+}
